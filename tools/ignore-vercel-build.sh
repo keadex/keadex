d4b0cbf941a0589cd3589954b1108db0c5beea7c
Keadex <12099977+keadex@users.noreply.github.com>
2022-05-08 00:18:48 +0200

    fix(common): 🐛 fixed ignore vercel build script

--- tools/ignore-vercel-build.sh
+++ tools/ignore-vercel-build.sh
@@ -12,7 +12,7 @@ yarn add -D typescript@$TS_VERSION --prefer-offline -W
 yarn add -D nx@$NX_BIN_VERSION --prefer-offline -W
 
 # Run the affected command, comparing latest commit to the one before that
-npx nx affected:apps --plain --base=main~1 --head=main | grep $APP -q
+yarn nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
 
 # Store result of the previous command (grep)
 IS_AFFECTED=$?
