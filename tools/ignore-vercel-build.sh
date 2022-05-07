633a9f165e6fc3c137d6ee1ab5f508f33a334548
Keadex <12099977+keadex@users.noreply.github.com>
2022-05-07 23:33:09 +0200

    ci(common): 🎡 fix ignore vercel build script

--- tools/ignore-vercel-build.sh
+++ tools/ignore-vercel-build.sh
@@ -1,26 +1,24 @@
 # Name of the app to check.
-echo "ciao jack"
-echo $APP_TO_CHECK
-# APP=$APP_TO_CHECK
+APP=$APP_TO_CHECK
 
-# # Determine version of Nx installed
-# NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
-# TS_VERSION=$(node -e "console.log(require('./package.json').devDependencies['typescript'])")
+# Determine version of Nx installed
+NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
+TS_VERSION=$(node -e "console.log(require('./package.json').devDependencies['typescript'])")
 
-# # Install @nrwl/workspace in order to run the affected command
-# yarn add -D @nrwl/workspace@$NX_VERSION --prefer-offline
-# yarn add -D typescript@$TS_VERSION --prefer-offline
+# Install @nrwl/workspace in order to run the affected command
+yarn add -D @nrwl/workspace@$NX_VERSION --prefer-offline
+yarn add -D typescript@$TS_VERSION --prefer-offline
 
-# # Run the affected command, comparing latest commit to the one before that
-# yarn nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
+# Run the affected command, comparing latest commit to the one before that
+yarn nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
 
-# # Store result of the previous command (grep)
-# IS_AFFECTED=$?
+# Store result of the previous command (grep)
+IS_AFFECTED=$?
 
-# if [ $IS_AFFECTED -eq 1 ]; then
-#   echo "🛑 - Build cancelled"
-#   exit 0
-# elif [ $IS_AFFECTED -eq 0 ]; then
-#   echo "✅ - Build can proceed"
-#   exit 1
+if [ $IS_AFFECTED -eq 1 ]; then
+  echo "🛑 - Build cancelled"
+  exit 0
+elif [ $IS_AFFECTED -eq 0 ]; then
+  echo "✅ - Build can proceed"
+  exit 1
 fi
