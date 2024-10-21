9bae2e0110c3243c352048d77dcb0993dec5833e
Keadex <12099977+keadex@users.noreply.github.com>
2024-10-22 01:29:24 +0200

    fix(libs/mina-live): fixed exports

--- libs/mina-live/index.ts
+++ libs/mina-live/index.ts
@@ -1,6 +1,6 @@
 import './src/styles/index.css'
-import { MinaLive } from './src/components/MinaLive/MinaLive'
 
+export { MinaLive } from './src/components/MinaLive/MinaLive'
 export type { MinaLiveProps } from './src/components/MinaLive/MinaLive'
 
-export default MinaLive
+export { minaMiddleware } from './src/nextjs/mina-middleware'
