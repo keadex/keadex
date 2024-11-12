a4c0c88e356365e18541ecc52b79fb2b6a9ffb59
Keadex <12099977+keadex@users.noreply.github.com>
2024-11-12 01:08:53 +0100

    Squashed commit of the following:
    commit 36f2e5f476cd651b35d3e0de2804c70f4888846e
    Author: Keadex <12099977+keadex@users.noreply.github.com>
    Date:   Tue Nov 12 01:02:08 2024 +0100
    
        feat(apps/keadex-battisti): add support for gh token and private repo
    
    commit b19cf674f0f7c3b0f0e9fae9b5a07f1d2c1def8e
    Author: Keadex <12099977+keadex@users.noreply.github.com>
    Date:   Tue Nov 12 01:01:30 2024 +0100
    
        feat(libs/mina-react): add support for gh token

--- apps/keadex-battisti/src/components/MinaShareDiagram/MinaShareDiagram.tsx
+++ apps/keadex-battisti/src/components/MinaShareDiagram/MinaShareDiagram.tsx
@@ -6,21 +6,21 @@ import { useSearchParams } from 'next/navigation'
 import { PropsWithChildren, memo, useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
 import { useTranslation } from '../../app/i18n/client'
+import { MinaReactProps } from '@keadex/mina-react-npm/src/components/MinaReact/MinaReact'
 
 export const MinaReact = dynamic(() => import('@keadex/mina-react-npm'), {
   ssr: false,
 })
 
-const MemoizedMinaReact = memo(
-  (props: { projectRootUrl: string; diagramUrl: string }) => {
-    return (
-      <MinaReact
-        projectRootUrl={props.projectRootUrl}
-        diagramUrl={props.diagramUrl}
-      />
-    )
-  },
-)
+const MemoizedMinaReact = memo((props: MinaReactProps) => {
+  return (
+    <MinaReact
+      projectRootUrl={props.projectRootUrl}
+      diagramUrl={props.diagramUrl}
+      ghToken={props.ghToken}
+    />
+  )
+})
 MemoizedMinaReact.displayName = 'MinaReact'
 
 export type MinaShareDiagramProps = {
@@ -30,6 +30,7 @@ export type MinaShareDiagramProps = {
 
 const PROJECT_ROOT_URL_PARAM_NAME = 'projectRootUrl'
 const DIAGRAM_URL_PARAM_NAME = 'diagramUrl'
+const GH_TOKEN_PARAM_NAME = 'ghToken'
 
 export default function MinaShareDiagram({
   children,
@@ -41,6 +42,7 @@ export default function MinaShareDiagram({
 
   const projectRootUrlParam = searchParams?.get(PROJECT_ROOT_URL_PARAM_NAME)
   const diagramUrlParam = searchParams?.get(DIAGRAM_URL_PARAM_NAME)
+  const ghTolenParam = searchParams?.get(GH_TOKEN_PARAM_NAME)
 
   const [projectRootUrl, setProjectRootUrl] = useState(
     !projectRootUrlParam || projectRootUrlParam.replace(/ /g, '').length === 0
@@ -52,9 +54,15 @@ export default function MinaShareDiagram({
       ? 'https://raw.githubusercontent.com/keadex/keadex/main/apps/keadex-diagrams/diagrams/container/keadex-mina'
       : decodeURI(diagramUrlParam),
   )
+  const [ghToken, setGhToken] = useState(
+    !ghTolenParam || ghTolenParam.replace(/ /g, '').length === 0
+      ? ''
+      : ghTolenParam,
+  )
 
   const [projectRootUrlRendered, setProjectRootUrlRendered] = useState('')
   const [diagramUrlRendered, setDiagramUrlRendered] = useState('')
+  const [ghTokenRendered, setGhTokenRendered] = useState('')
 
   function isButtonDisabled() {
     return (
@@ -67,6 +75,7 @@ export default function MinaShareDiagram({
     if (projectRootUrl && diagramUrl) {
       setProjectRootUrlRendered(projectRootUrl)
       setDiagramUrlRendered(diagramUrl)
+      setGhTokenRendered(ghToken)
     }
   }
 
@@ -75,12 +84,19 @@ export default function MinaShareDiagram({
       const projectRootUrlParam = `${PROJECT_ROOT_URL_PARAM_NAME}=${encodeURI(
         projectRootUrl,
       )}`
-      const diagramUrlParam = `${DIAGRAM_URL_PARAM_NAME}=${encodeURI(
+      const diagramUrlParam = `&${DIAGRAM_URL_PARAM_NAME}=${encodeURI(
         diagramUrl,
       )}`
-      const link = `${window.location.origin}${window.location.pathname}?${projectRootUrlParam}&${diagramUrlParam}`
+      const ghTokenParam = `&${GH_TOKEN_PARAM_NAME}=${ghToken}`
+      const link = `${window.location.origin}${
+        window.location.pathname
+      }?${projectRootUrlParam}${diagramUrlParam}${ghToken ? ghTokenParam : ''}`
       navigator.clipboard.writeText(link)
       toast.info(t('keadex_mina.share_diagram.link_copied'))
+      if (ghToken)
+        toast.warn(t('keadex_mina.share_diagram.gh_token_copy_warning'), {
+          autoClose: false,
+        })
     }
   }
 
@@ -103,6 +119,13 @@ export default function MinaShareDiagram({
         info={`*${t('common.required')}`}
         classNameRoot="mt-5"
       />
+      <Input
+        value={ghToken}
+        onChange={(e) => setGhToken(e.target.value)}
+        label={t('common.gh_token')}
+        info={t('keadex_mina.share_diagram.gh_token_info')}
+        classNameRoot="mt-5"
+      />
       <div className="flex flex-col md:flex-row mx-auto mt-5">
         <Button
           disabled={isButtonDisabled()}
@@ -123,6 +146,7 @@ export default function MinaShareDiagram({
         <MemoizedMinaReact
           projectRootUrl={projectRootUrlRendered}
           diagramUrl={diagramUrlRendered}
+          ghToken={ghTokenRendered}
         />
       </div>
     </div>
