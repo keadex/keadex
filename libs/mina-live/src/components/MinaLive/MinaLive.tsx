2b995f3ecdccb17d0352828a8dc13c804121df3d
Keadex <12099977+keadex@users.noreply.github.com>
2025-02-18 19:33:08 +0100

    refactor(libs/mina-live): add mina app wasm initialization logic

--- libs/mina-live/src/components/MinaLive/MinaLive.tsx
+++ libs/mina-live/src/components/MinaLive/MinaLive.tsx
@@ -2,7 +2,7 @@
 
 import '../../tauri/tauri-web-adapter'
 import useEventEmitter from 'ahooks/lib/useEventEmitter'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
 import {
@@ -21,6 +21,8 @@ import { router } from '@keadex/keadex-mina/src/core/router/router'
 import store from '@keadex/keadex-mina/src/core/store/store'
 import initi18n from '@keadex/keadex-mina/src/i18n'
 import '@keadex/keadex-mina/src/styles/index.css'
+import { init_app } from '../../../src-rust/pkg'
+import { useForceUpdate } from '@keadex/keadex-ui-kit/cross'
 
 initi18n({
   backend: {
@@ -32,8 +34,12 @@ initi18n({
 export type MinaLiveProps = {}
 
 export const MinaLive = React.memo<MinaLiveProps>((props) => {
+  // minaAppInitialized must be a ref since we have to be sure of initializing the Rust Mina app
+  // only once. Initializing it multiple times will throw an error due to some Rust dependencies.
+  const minaAppInitialized = useRef(false)
   const teInitialized = useRef(false)
   const event$ = useEventEmitter<AppEvent>()
+  const { forceUpdate } = useForceUpdate()
 
   useEffect(() => {
     // setTimeout() needed due to tw-elements@1.0.0-beta2 bug:
@@ -46,14 +52,30 @@ export const MinaLive = React.memo<MinaLiveProps>((props) => {
       }
       teInitialized.current = true
     }, 1000)
+
+    if (!minaAppInitialized.current) {
+      init_app()
+      console.debug(`Mina app initialized`)
+      forceUpdate()
+    }
+    minaAppInitialized.current = true
   }, [])
 
   return (
-    <Provider store={store}>
-      <AppEventContext.Provider value={event$}>
-        <RouterProvider router={router} />
-      </AppEventContext.Provider>
-    </Provider>
+    <>
+      {minaAppInitialized.current && (
+        <Provider store={store}>
+          <AppEventContext.Provider value={event$}>
+            <RouterProvider router={router} />
+          </AppEventContext.Provider>
+        </Provider>
+      )}
+      {!minaAppInitialized.current && (
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-xl">
+          Mina is loading...
+        </div>
+      )}
+    </>
   )
 })
 
