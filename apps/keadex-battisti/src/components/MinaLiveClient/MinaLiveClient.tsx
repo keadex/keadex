1a319800447abbf1bfba34960d51899ee1ff1e28
Keadex <12099977+keadex@users.noreply.github.com>
2025-02-19 23:25:50 +0100

    feat(apps/keadex-battisti): enable mina component

--- apps/keadex-battisti/src/components/MinaLiveClient/MinaLiveClient.tsx
+++ apps/keadex-battisti/src/components/MinaLiveClient/MinaLiveClient.tsx
@@ -1,18 +1,14 @@
 'use client'
 
-import { PropsWithChildren } from 'react'
 import dynamic from 'next/dynamic'
 
-// const MinaLive = dynamic(() => import('@keadex/mina-live/MinaLive'), {
-//   ssr: false,
-// })
+const MinaLive = dynamic(() => import('@keadex/mina-live/MinaLive'), {
+  ssr: false,
+})
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type MinaLiveClientProps = {}
 
-export default function MinaLiveClient({
-  children,
-}: PropsWithChildren<MinaLiveClientProps>) {
-  // return <MinaLive />
-  return <div></div>
+export default function MinaLiveClient() {
+  return <MinaLive />
 }
