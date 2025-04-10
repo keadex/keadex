e9ec41912fcb3c61342ed6175f2ce418ffe1fec6
Keadex <12099977+keadex@users.noreply.github.com>
2025-04-11 00:54:34 +0200

    fix(libs/mina-confluence-plugin): upgrade to latest forge APIs

---
+++ libs/mina-confluence-plugin/src/useSubmit.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { view } from '@forge/bridge'
+import { MinaMacroConfig } from './Config'
+
+export const useSubmit = () => {
+  const [error, setError] = useState(false)
+  const [message, setMessage] = useState('')
+
+  const submit = async (fields: MinaMacroConfig) => {
+    const payload = { config: fields }
+
+    try {
+      await view.submit(payload)
+      setError(false)
+      setMessage(`Submitted successfully.`)
+    } catch (error: any) {
+      setError(true)
+      setMessage(`${error.code}: ${error.message}`)
+    }
+  }
+
+  return {
+    error,
+    message,
+    submit,
+  }
+}
