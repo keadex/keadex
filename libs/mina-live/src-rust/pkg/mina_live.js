f5ed0882e8dddd990a84ab234f21f4af0d1c1547
Keadex <12099977+keadex@users.noreply.github.com>
2025-04-21 00:23:01 +0200

    refactor(libs/mina-live): untrack autogenerated code

--- libs/mina-live/src-rust/pkg/mina_live.js
+++
@@ -1,5 +0,0 @@
-import * as wasm from "./mina_live_bg.wasm";
-export * from "./mina_live_bg.js";
-import { __wbg_set_wasm } from "./mina_live_bg.js";
-__wbg_set_wasm(wasm);
-wasm.__wbindgen_start();
