20daf49176ad1b2199ef77ffc500dbf9ec86865b
Keadex <12099977+keadex@users.noreply.github.com>
2025-05-17 10:16:18 +0200

    feat(libs/keadex-nx-plugin): add custom changelog renderer

---
+++ libs/keadex-nx-plugin/src/tools/changelog-renderer.ts
@@ -0,0 +1,21 @@
+import DefaultChangelogRenderer from 'nx/release/changelog-renderer'
+
+export default class VersionPlanChangelogRenderer extends DefaultChangelogRenderer {
+  constructor(config) {
+    super(config)
+  }
+
+  render(): Promise<string> {
+    let changes = ''
+    const changelogs = this.changes[0].description.split(/^# (.*)/gm)
+    const changelogIndex = changelogs.findIndex((val) => val === this.project)
+    if (changelogIndex !== -1 && changelogIndex + 1 < changelogs.length) {
+      changes = changelogs[changelogIndex + 1]
+    }
+
+    const changelog = `
+# ${this.project}@${this.changelogEntryVersion}${changes}
+    `
+    return Promise.resolve(changelog)
+  }
+}
