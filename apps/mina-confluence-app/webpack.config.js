16556e63e33278b92ba5b900f2e6527d4ab37625
Keadex <12099977+keadex@users.noreply.github.com>
2025-06-24 02:21:42 +0200

    fix(libs/mina-confluence-plugin): fix build

--- apps/mina-confluence-app/webpack.config.js
+++ apps/mina-confluence-app/webpack.config.js
@@ -1,8 +1,25 @@
 const { composePlugins, withNx } = require('@nx/webpack')
 const { withReact } = require('@nx/react')
+const { DefinePlugin } = require('webpack')
+const { readFileSync } = require('fs')
+const { join } = require('path')
 
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   config.experiments.asyncWebAssembly = true
+
+  config.plugins = [
+    ...(config.plugins ?? []),
+    new DefinePlugin({
+      'import.meta.env': {
+        VITE_AI_ENABLED: JSON.stringify(true),
+        VITE_WEB_MODE: JSON.stringify(true),
+        VITE_APP_VERSION: JSON.stringify(
+          JSON.parse(readFileSync(join(__dirname, 'package.json')).toString())
+            .version,
+        ),
+      },
+    }),
+  ]
 
   return config
 })
