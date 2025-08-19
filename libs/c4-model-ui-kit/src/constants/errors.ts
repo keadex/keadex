9c8e7225b83c55ee4a0b3454251007d15a73331a
Keadex <12099977+keadex@users.noreply.github.com>
2025-08-19 23:44:53 +0200

    Mina 2.5.1 - What's New:
    Fixes:
      - Added Graphviz exception handler

--- libs/c4-model-ui-kit/src/constants/errors.ts
+++ libs/c4-model-ui-kit/src/constants/errors.ts
@@ -1,5 +1,5 @@
 export const WARN_TOO_MANY_ELEMENTS =
-  'The diagram contains too many elements and relationships, which may cause the auto layout to break. Try adjusting auto layout settings like ranksep or subgraph margins.'
+  'The diagram contains too many elements and relationships, which may cause the auto layout to break. Try adjusting auto layout settings like orientation, ranksep or subgraph margins.'
 export const ERROR_GRAPHVIZ_NOT_INITIALIZED =
   'Graphviz not initialized. Cannot generate auto layout.'
 export const ERROR_TRIANGULATION_FAILED = 'triangulation failed'
