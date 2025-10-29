06eebb90eba186c87b64554ca162733205076446
Keadex <12099977+keadex@users.noreply.github.com>
2025-10-29 23:19:02 +0100

    fix(apps/keadex-battisti): fix nextjs type

--- apps/keadex-battisti/next-env.d.ts
+++ apps/keadex-battisti/next-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 /// <reference types="next/navigation-types/compat/navigation" />
-/// <reference path="./.next/types/routes.d.ts" />
+/// <reference path="./../../dist/apps/keadex-battisti/.next/types/routes.d.ts" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
