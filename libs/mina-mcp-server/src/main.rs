e4c348ea013b1cd404fa5943943af5c67c62b4ca
Keadex <12099977+keadex@users.noreply.github.com>
2026-03-06 00:58:35 +0100

    feat(libs/mina-mcp-server): add list diagrams tool

--- libs/mina-mcp-server/src/main.rs
+++ libs/mina-mcp-server/src/main.rs
@@ -1,10 +1,11 @@
 pub mod models;
 pub mod tools;
 
+use crate::tools::local_diagrams_tools::list_diagrams_tool;
 use crate::tools::local_diagrams_tools::read_diagram_tool;
 use anyhow::Result;
-use keadex_mina::error_handling::mina_error::MinaError;
 use keadex_mina::model::diagram::Diagram;
+use keadex_mina::{error_handling::mina_error::MinaError, model::diagram::DiagramType};
 use models::requests::read_local_diagram_request::ReadLocalDiagramRequest;
 use rmcp::{
   ServerHandler, ServiceExt,
@@ -12,6 +13,7 @@ use rmcp::{
   model::*,
   tool, tool_handler, tool_router,
 };
+use std::collections::HashMap;
 
 pub struct KeadexMina {
   tool_router: ToolRouter<KeadexMina>,
@@ -33,6 +35,16 @@ impl KeadexMina {
     Parameters(request): Parameters<ReadLocalDiagramRequest>,
   ) -> Result<Json<Diagram>, Json<MinaError>> {
     read_diagram_tool(self, request).await
+  }
+
+  #[tool(
+    description = "Retrieve the list of diagrams in a local Keadex Mina project. Use this tool when you need to list all the architectural diagrams present in a Keadex Mina project. More specifically, this tool returns a map where the keys are the C4 Model diagram types and the values are vectors of strings representing the names of the diagrams. Do not use this tool for remote Keadex Mina projects that have not been cloned locally."
+  )]
+  async fn list_local_diagrams(
+    &self,
+    Parameters(request): Parameters<ReadLocalDiagramRequest>,
+  ) -> Result<Json<HashMap<DiagramType, Vec<String>>>, Json<MinaError>> {
+    list_diagrams_tool(self, request).await
   }
 }
 
--- libs/mina-mcp-server/src/tools/local_diagrams_tools.rs
+++ libs/mina-mcp-server/src/tools/local_diagrams_tools.rs
@@ -1,11 +1,13 @@
 use crate::KeadexMina;
 use crate::models::requests::read_local_diagram_request::ReadLocalDiagramRequest;
 use anyhow::Result;
-use keadex_mina::error_handling::mina_error::MinaError;
 use keadex_mina::model::diagram::Diagram;
+use keadex_mina::repository::diagram_repository::list_diagrams;
 use keadex_mina::service::diagram_service::get_diagram;
+use keadex_mina::{error_handling::mina_error::MinaError, model::diagram::DiagramType};
 use mina_cli::helpers::mina_lifecycle_helper::{clear_keadex_mina, init_keadex_mina};
 use rmcp::handler::server::wrapper::Json;
+use std::collections::HashMap;
 use std::path::PathBuf;
 
 pub async fn read_diagram_tool(
@@ -20,4 +22,16 @@ pub async fn read_diagram_tool(
     .map_err(|e| Json(e))?;
   clear_keadex_mina(&PathBuf::from(&request.mina_project_path)).await;
   Ok(Json(diagram))
+}
+
+pub async fn list_diagrams_tool(
+  _router: &KeadexMina,
+  request: ReadLocalDiagramRequest,
+) -> Result<Json<HashMap<DiagramType, Vec<String>>>, Json<MinaError>> {
+  init_keadex_mina(&PathBuf::from(&request.mina_project_path))
+    .await
+    .map_err(|e| Json(e))?;
+  let diagrams = list_diagrams().await.map_err(|e| Json(e))?;
+  clear_keadex_mina(&PathBuf::from(&request.mina_project_path)).await;
+  Ok(Json(diagrams))
 }
