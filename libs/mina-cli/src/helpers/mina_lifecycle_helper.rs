247e37abbd07c63d3671fbb08dcc740c87bb407a
Keadex <12099977+keadex@users.noreply.github.com>
2026-03-09 01:51:37 +0100

    refactor(libs/mina-cli): return project

--- libs/mina-cli/src/helpers/mina_lifecycle_helper.rs
+++ libs/mina-cli/src/helpers/mina_lifecycle_helper.rs
@@ -1,11 +1,12 @@
 use keadex_mina::core::project_initializer::{load_project, unload_project};
 use keadex_mina::error_handling::mina_error::MinaError;
+use keadex_mina::model::project::Project;
 use std::path::PathBuf;
 
-pub async fn init_keadex_mina(project_path: &PathBuf) -> Result<(), MinaError> {
+pub async fn init_keadex_mina(project_path: &PathBuf) -> Result<Project, MinaError> {
   let _app = keadex_mina::core::app::App::new();
-  let _project = load_project(project_path.to_str().unwrap()).await?;
-  return Ok(());
+  let project = load_project(project_path.to_str().unwrap()).await?;
+  return Ok(project);
 }
 
 pub async fn clear_keadex_mina(project_path: &PathBuf) {
